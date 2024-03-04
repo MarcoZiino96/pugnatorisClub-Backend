8c9331b108036e89f9e681d79241f779fc3b546b
MarcoZiino96 <141355693+MarcoZiino96@users.noreply.github.com>
2024-03-04 12:37:49 +0100

    Initial commit

---
+++ src/main/java/it/epicode/pugnatorisClub/PugnatorisClubApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.pugnatorisClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PugnatorisClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PugnatorisClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/pugnatorisClub/PugnatorisClubApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.pugnatorisClub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PugnatorisClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
